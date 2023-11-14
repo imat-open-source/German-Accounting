8e46f2f19dc49ffa9e33b6bb79bddf9b79d29b4a
beingeek <furqan.790@outlook.com>
2023-11-14 18:20:49 +0500

    chore: refactoring for reducing complexity

--- german_accounting/events/extended_tax_category.py
+++ german_accounting/events/extended_tax_category.py
@@ -2,6 +2,7 @@ import frappe
 from frappe.utils import flt
 from frappe.utils.nestedset import get_descendants_of
 
+
 def validate_tax_category_fields(doc, method=None):
     goods_amt_sum = 0.0
     services_amt_sum = 0.0
@@ -11,16 +12,16 @@ def validate_tax_category_fields(doc, method=None):
         descendants_item_group = get_descendants_of("Item Group", item_group)
         return item_groups + descendants_item_group if descendants_item_group else item_groups
 
-    goods_item_group = frappe.get_cached_value('German Accounting Settings', None, 'goods_item_group')
-    service_item_group = frappe.get_cached_value('German Accounting Settings', None, 'service_item_group')
-    if not goods_item_group:
+    german_accounting_settings = frappe.get_cached_doc('German Accounting Settings')
+
+    if not german_accounting_settings.goods_item_group:
         frappe.throw('Please set Goods Item Group in German Accounting Settings')
 
-    if not service_item_group:
+    if not german_accounting_settings.service_item_group:
         frappe.throw('Please set Service Item Group in German Accounting Settings')
 
-    goods_item_group_list = get_parent_and_descendants_item_group_list(goods_item_group)
-    services_item_group_list = get_parent_and_descendants_item_group_list(service_item_group)
+    goods_item_group_list = get_parent_and_descendants_item_group_list(german_accounting_settings.goods_item_group)
+    services_item_group_list = get_parent_and_descendants_item_group_list(german_accounting_settings.service_item_group)
     # Here we go through the item table and count the amounts for the two categories
     for item in doc.get("items"):
         if item.item_group in goods_item_group_list:
@@ -28,8 +29,8 @@ def validate_tax_category_fields(doc, method=None):
         elif item.item_group in services_item_group_list:
             services_amt_sum += flt(item.amount)
 
-    # Test which amount is higher...
-    if goods_amt_sum >= services_amt_sum:
-        doc.item_group = goods_item_group
+    if goods_amt_sum > services_amt_sum:
+        doc.item_group = german_accounting_settings.goods_item_group
+
     else:
-        doc.item_group = service_item_group
+        doc.item_group = german_accounting_settings.service_item_group
