f830e10551cc74150a610691ff9fdda87b2f925e
beingeek <furqan.790@outlook.com>
2023-11-22 14:38:55 +0500

    feat: In quotation conditionally fetch customer_type from lead or customer (party_name)

--- german_accounting/setup/install.py
+++ german_accounting/setup/install.py
@@ -51,11 +51,12 @@ def get_custom_fields():
 		{
 			"label": "Customer Type",
 			"fieldname": "customer_type",
-			"fieldtype": "Data",
+			"fieldtype": "Select",
+			"options": "\nCompany\nIndividual",
+			"default": "",
 			"read_only": 1,
 			"translatable": 0,
 			"insert_after": "vat_id",
-			"fetch_from": "party_name.customer_type",
 		}
 	]
 
