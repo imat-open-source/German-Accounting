03a8f3aeb1eb978468c452f359877a3f12990d2c
Muhammad Ali <swe.mirza.ali@gmail.com>
2024-03-21 17:31:50 +0000

    Issue#18 | Added Fixture for property setter in address for billing address description

--- german_accounting/hooks.py
+++ german_accounting/hooks.py
@@ -10,6 +10,19 @@ app_license = "MIT"
 # Includes in <head>
 # ------------------
 
+fixtures = [
+	{
+		"dt": "Property Setter",
+		"filters": {
+			"name": (
+				"in",
+				[
+					"Address-is_primary_address-description"
+				],
+			)
+		},
+	},
+]
 # include js, css files in header of desk.html
 # app_include_css = "/assets/german_accounting/css/german_accounting.css"
 # app_include_js = "/assets/german_accounting/js/german_accounting.js"
